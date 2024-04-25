f0ce3d94e1e734a811a04ce2201435529078b1a7
Vi Long Luong <vi-long.luong@epita.fr>
2024-04-25 12:29:47 +0200

    Modify the code to adapt all combinations

--- main.py
+++ main.py
@@ -25,20 +25,28 @@ def main():
     # vote_system.display_votes()
 
     for voter, public_sign_key in vote_system.voters:
+
+        # Encrypt vote
         encrypted_vote = vote_system.encrypt_vote(voter.voting())
+
         for ballot in encrypted_vote:
+
+            # Aggregate vote
             c1, c2 = ballot
-            if vote_system.sign_method == 'DSA':
+            if vote_system.encrypt_method == 'ElGamal':
                 b = int_to_bytes(c1) + int_to_bytes(c2)
-            elif vote_system.sign_method == 'ECDSA':
+            elif vote_system.encrypt_method == 'EC_ElGamal':
                 c = add(c1[0], c1[1], c2[0], c2[1], p)
                 b = int_to_bytes(c[0]) + int_to_bytes(c[1])
+
             # Voter signs its ballot
             r, s = voter.sign(b, vote_system.sign_method)
+
             # Verify signature
             if not(vote_system.verify_vote((r, s), b, public_sign_key)):
                 print("Signature is not verified !!!")
                 exit(1)
+
         # If all ballots signature have been verified, add the vote to the system
         vote_system.add_vote(encrypted_vote)
     
